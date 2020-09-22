233223f03a9926b89bae37e27a2b627fb71a639b
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-22 19:16:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,7 +29,7 @@ class Artist
     #   # artist
     # end
     # artist
-    artist = @@all.find { |artist| artist.name == name} ? artist : Artist.new(name)
+    !!@@all.find { |artist| artist.name == name} ? @@all.find { |artist| artist.name == name} : Artist.new(name)
   end
 
   def print_songs
